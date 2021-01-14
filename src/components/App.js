503b104f5d2178c0e5ecb61cc11ac24e4bdf51fc
Justice M <codenamesolutions@gmail.com>
2021-01-14 23:06:02 +0200

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div>
+      <Switch>
+        
+      </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
